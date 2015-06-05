d2b17ec92920184a2a29cac6c0463c044e4630f9
Kevin Chen <kvchen@berkeley.edu>
2015-06-05 00:40:04 -0700

    Add encrypted project secrets for Travis

---
+++ encrypt_secrets.sh
@@ -0,0 +1,10 @@
+declare -a SECRETS=(
+	"app/config/app.coffee"
+	"app/config/database.coffee"
+	"app/config/oauth.coffee"
+)
+
+tar cjf secrets.tar "${SECRETS[@]}"
+travis encrypt-file secrets.tar --add
+
+
